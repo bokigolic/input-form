3eb9ddf22e5a0cc10d99251421dbb0aaaf7df50c
Bojan Golic <bokigolic32@gmail.com>
2022-08-12 23:05:35 -0400

    preset and state done

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,9 +1,14 @@
+import { useState } from "react";
+
 const Form = () => {
 
   const preset = {
+    username: "Boki",
+    password: "123",
 
   }
 
+  const [forma, setForma] = useState(preset);
 
 
   return (
@@ -11,20 +16,20 @@ const Form = () => {
       <div>
         <h2>Test  FORM</h2>
 
-       <div>
-       <label>Name</label>
-        <input
-          type="text"
-          name="name"
-        />
-       </div>
-      <div>
-      <label>Password</label>
-        <input
-          type="password"
-          name="password"
-        />
-      </div>
+        <div>
+          <label>Name</label>
+          <input
+            type="text"
+            name="name"
+          />
+        </div>
+        <div>
+          <label>Password</label>
+          <input
+            type="password"
+            name="password"
+          />
+        </div>
       </div>
     </form>
   )
